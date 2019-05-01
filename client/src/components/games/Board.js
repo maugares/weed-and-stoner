2b3e5d5a4110bf5dbbb35d6d2594d90331451ad8
maugares <mauro.garciaesteban@gmail.com>
2019-05-01 16:46:25 +0200

    adjust Board.js to props being game (vs board)

--- client/src/components/games/Board.js
+++ client/src/components/games/Board.js
@@ -12,7 +12,7 @@ const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
   )
 }
 
-export default ({board, makeMove}) => board.map((cells, rowIndex) =>
+export default ({game, makeMove}) => game.board.map((cells, rowIndex) =>
   <div key={rowIndex}>
     {cells.map((symbol, cellIndex) => renderCel(makeMove, rowIndex, cellIndex,symbol,false))}
   </div>
