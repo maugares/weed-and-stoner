05e893d67242a87f7b0dcdca4cc5205a7dbc551f
maugares <mauro.garciaesteban@gmail.com>
2019-05-01 11:45:25 +0200

    add file Validation.js

--- client/src/components/games/GameDetails.js
+++ client/src/components/games/GameDetails.js
@@ -1,9 +1,9 @@
-import React, {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import {getGames, joinGame, updateGame} from '../../actions/games'
-import {getUsers} from '../../actions/users'
-import {userId} from '../../jwt'
+import React, { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { getGames, joinGame, updateGame } from '../../actions/games'
+import { getUsers } from '../../actions/users'
+import { userId } from '../../jwt'
 import Paper from '@material-ui/core/Paper'
 import Board from './Board'
 import './GameDetails.css'
@@ -28,17 +28,16 @@ class GameDetails extends PureComponent {
         else return cell
       })
     )
-    updateGame(game.id, board)
+
+    updateGame(game.id, game.board)
   }
 
-
-
   render() {
-    const {game, users, authenticated, userId} = this.props
+    const { game, users, authenticated, userId } = this.props
 
     if (!authenticated) return (
-			<Redirect to="/login" />
-		)
+      <Redirect to="/login" />
+    )
 
     if (game === null || users === null) return 'Loading...'
     if (!game) return 'Not found'
