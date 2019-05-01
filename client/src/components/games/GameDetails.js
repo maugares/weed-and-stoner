0f2a3ba70fb0d89f1aaad2309b4a36782b0663dc
maugares <mauro.garciaesteban@gmail.com>
2019-05-01 11:47:27 +0200

    import findX function in GameDetails

--- client/src/components/games/GameDetails.js
+++ client/src/components/games/GameDetails.js
@@ -7,6 +7,8 @@ import { userId } from '../../jwt'
 import Paper from '@material-ui/core/Paper'
 import Board from './Board'
 import './GameDetails.css'
+import { findX } from './Validation'
+
 
 class GameDetails extends PureComponent {
 
