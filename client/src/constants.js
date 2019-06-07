1a50e8ee47f758d3f4eaafe15508c5b9b077846a
maugares <mauro.garciaesteban@gmail.com>
2019-06-07 02:11:05 +0200

    replace baseUrl

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = 'https://git.heroku.com/ancient-brook-26494.git/users' || 'http://localhost:4000'
 
 export const localStorageJwtKey = 'currentUserJwt'
