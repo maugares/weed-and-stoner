b9976ce1e1fb8b4643fe4bbdd524f09ca810f433
maugares <mauro.garciaesteban@gmail.com>
2019-06-12 11:40:34 +0200

    replace baseUrl in constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,4 @@
-export const baseUrl = 'https://git.heroku.com/ancient-brook-26494.git' || 'http://localhost:4000'
+export const baseUrl = 'https://fathomless-river-81560.herokuapp.com'
+// export const baseUrl = 'http://localhost:4000'
 
 export const localStorageJwtKey = 'currentUserJwt'
